ffda468d8f944e71e3e06748b62f70e85bf2f66f
Karlo Zivkovic <karlo.zivkovic2@gmail.com>
2024-05-18 12:04:36 +0200

    Ready for push!

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use errors::EzyTutorError;
+use sqlx::postgres::PgPool;
+use std::env;
+use std::io;
+use std::sync::Mutex;
+
+#[path = "./dbaccess/mod.rs"]
+mod dbaccess;
+#[path = "./errors.rs"]
+mod errors;
+#[path = "./handlers/mod.rs"]
+mod handlers;
+#[path = "./models/mod.rs"]
+mod models;
+#[path = "./routes.rs"]
+mod routes;
+#[path = "./state.rs"]
+mod state;
+
+use routes::*;
+use state::AppState;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You've already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .app_data(web::JsonConfig::default().error_handler(|_err, _req| {
+                EzyTutorError::InvalidInput("Please provide valid Json input".to_string()).into()
+            }))
+            .configure(general_routes)
+            .configure(course_routes)
+            .configure(tutor_routes)
+    };
+    let host_port = env::var("HOST_PORT").expect("HOST:PORT address is not set in .env file");
+
+    HttpServer::new(app).bind(&host_port)?.run().await
+}
